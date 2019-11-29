23c3f6f981a8b55be40b0c11f625bdcadec80be4
pycaret <58118658+pycaret@users.noreply.github.com>
2019-11-29 12:32:16 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="pycaret",
+    version="0.0.1",
+    description="A Python package for supervised and unsupervised machine learning.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/pycaret/pycaret",
+    author="Moez Ali",
+    author_email="moez.ali@queensu.ca",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["pycaret"],
+    include_package_data=True,
+    install_requires=["sys", "warnings", "pandas", "numpy", "seaborn", "matplotlib", "IPython", "joblib", 
+                     "sklearn", "shap", "ipywidgets", "random", "time", "datetime"]
+)
