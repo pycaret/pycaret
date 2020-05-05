c413125fe4d8ab3a004c7754096503e268d23b24
Moez Ali <mali085@ca.nam.ad.pwcinternal.com>
2020-05-05 16:12:33 -0400

    updated setup.py and .travis.yml

--- setup.py
+++ setup.py
@@ -23,7 +23,8 @@ setup(
     license="MIT",
     classifiers=[
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
     ],
     include_package_data=True,
