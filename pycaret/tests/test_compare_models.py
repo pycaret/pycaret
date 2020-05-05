b737349891ab14006ce77849f0077345ffb2bb11
Moez Ali <mali085@ca.nam.ad.pwcinternal.com>
2020-05-05 10:15:49 -0400

    test created

--- pycaret/tests/test_compare_models.py
+++ pycaret/tests/test_compare_models.py
@@ -1,1 +1,11 @@
-#placeholder
+#compare_models_test
+import pytest
+
+def test_compare_models():
+    from pycaret.datasets import get_data
+    data = get_data('boston')
+    from pycaret.regression import *
+    reg1 = setup(data, target='medv',silent=True, html=False, session_id=123)
+    models = compare_models(n_select=3)
+    top_3 = len(models)
+    assert top_3 == 3
