062360e0ad01d20c5d76e499be6cd3511d86ab4c
Moez Ali <mali085@ca.nam.ad.pwcinternal.com>
2020-05-05 13:07:50 -0400

    updated test file

--- pycaret/tests/test_compare_models.py
+++ pycaret/tests/test_compare_models.py
@@ -3,12 +3,12 @@ sys.path.insert(0, os.path.abspath(".."))
 
 #compare_models_test
 import pytest
-import regression
-import datasets
+import pycaret.regression
+import pycaret.datasets
 
 def test_compare_models():
-    data = datasets.get_data('boston')
-    reg1 = regression.setup(data, target='medv',silent=True, html=False, session_id=123)
-    models = regression.compare_models(n_select=3)
+    data = pycaret.datasets.get_data('boston')
+    reg1 = pycaret.regression.setup(data, target='medv',silent=True, html=False, session_id=123)
+    models = pycaret.regression.compare_models(n_select=3)
     top_3 = len(models)
     assert top_3 == 3
