c35af19cc359259d73a6fc2e6f7f4c26cfb3a2bf
Moez Ali <mali085@ca.nam.ad.pwcinternal.com>
2020-05-05 16:18:35 -0400

    test case updated

--- setup.py
+++ setup.py
@@ -27,6 +27,6 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
     ],
-    include_package_data=True,
+    include_package_data=True, 
     install_requires=required
 )
