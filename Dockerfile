57a1c859d37919b1dca0b7b13e0caf37246d1def
PyCaret <moez@pycaret.org>
2020-07-12 02:24:30 -0400

    updated Dockerfile and main.py

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,8 @@ RUN apt-get update && apt-get install -y libgomp1
 
 RUN pip install --trusted-host pypi.python.org -r requirements.txt
 
+RUN pip install Flask
+
 ENTRYPOINT ["python"]
 
 CMD ["/app/main.py"]
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 # main.py
 # entry point for Dockerfile
 
-def main():
+from flask import Flask
+main = Flask(__name__)
 
+@app.route('/')
+def main():
     from pycaret.utils import version
-    print('Success')
-    return version()
+    return version
+
+if __name__ == '__main__':
+    main.run(host='0.0.0.0', debug = True)
