c8129498b2b4947e400479e58de1ca1a347ce28b
PyCaret <moez@pycaret.org>
2020-07-17 14:07:58 -0400

    update tune_model test case

--- pycaret/tests/test_tune_model.py
+++ pycaret/tests/test_tune_model.py
@@ -10,21 +10,21 @@ def test_model_tuning_r2():
     data = pycaret.datasets.get_data('boston')
     data = data.head(50)
     reg1 = pycaret.regression.setup(data, target='medv',silent=True, verbose=False, html=False, session_id=123)
-    model = pycaret.regression.tune_model(pycaret.regression.create_model('rf', verbose = False), verbose=False, optimize = 'r2')
+    model = pycaret.regression.tune_model(pycaret.regression.create_model('rf', verbose = False), verbose=False, optimize = 'R2')
     assert hasattr(model, 'predict')
 
 def test_model_tuning_mae():
     data = pycaret.datasets.get_data('boston')
     data = data.head(50)
     reg1 = pycaret.regression.setup(data, target='medv',silent=True, verbose=False, html=False, session_id=123)
-    model = pycaret.regression.tune_model(pycaret.regression.create_model('ada', verbose = False), verbose=False, optimize = 'mae')
+    model = pycaret.regression.tune_model(pycaret.regression.create_model('ada', verbose = False), verbose=False, optimize = 'MAE')
     assert hasattr(model, 'predict')
 
 def test_model_tuning_mse():
     data = pycaret.datasets.get_data('boston')
     data = data.head(50)
     reg1 = pycaret.regression.setup(data, target='medv',silent=True, verbose=False, html=False, session_id=123)
-    model = pycaret.regression.tune_model(pycaret.regression.create_model('lr', verbose = False), verbose=False, optimize = 'mse')
+    model = pycaret.regression.tune_model(pycaret.regression.create_model('lr', verbose = False), verbose=False, optimize = 'MSE')
     assert hasattr(model, 'predict')
 
 
