da88440bf4f45daacbb6bdb2da5945301ef8bce3
Antoni Baum <antoni.baum@protonmail.com>
2020-08-07 20:56:36 +0200

    Regenerate and configure sphinx

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Moez Ali'
 author = 'Moez Ali'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.1'
+release = '2.0.0'
 
 
 # -- General configuration ---------------------------------------------------
