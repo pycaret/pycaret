38a51916730fd626761621dd16fc543de7221820
Daiki Katsuragawa <50144563+daikikatsuragawa@users.noreply.github.com>
2020-08-12 00:18:26 +0900

    Update test_anomaly.py

--- pycaret/tests/test_anomaly.py
+++ pycaret/tests/test_anomaly.py
@@ -11,7 +11,7 @@ def test():
     data = pycaret.datasets.get_data('anomaly')
 
     # init setup
-    ano1 = pycaret.anomaly.setup(data, normalize = True, silent=True, html=False, session_id=123)
+    ano1 = pycaret.anomaly.setup(data, normalize=True, silent=True, html=False, session_id=123)
 
     # create model
     iforest = pycaret.anomaly.create_model('iforest')
@@ -20,10 +20,29 @@ def test():
     # assign model
     iforest_results = pycaret.anomaly.assign_model(iforest)
     knn_results = pycaret.anomaly.assign_model(knn)
+    
+    # predict model
+    iforest_predictions = pycaret.anomaly.predict_model(model=iforest, data=data)
+    knn_predictions = pycaret.anomaly.predict_model(model=knn, data=data)
 
     # get config
     X = pycaret.anomaly.get_config('X')
     seed = pycaret.anomaly.get_config('seed')
+    
+    # set config
+    pycaret.anomaly.set_config('seed', 123) 
+    
+    # get logs
+    logs = pycaret.anomaly.get_logs()
+    
+    # save model
+    pycaret.anomaly.save_model(knn, 'knn_model_23122019')
+    
+    # load model
+    saved_knn = pycaret.anomaly.load_model('knn_model_23122019')
+    
+    # returns table of models
+    all_models = pycaret.anomaly.models()
 
     assert 1 == 1
 
