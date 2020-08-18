189ef4d4119ac64e2f56a23cc91bef028330a3db
Antoni Baum <antoni.baum@protonmail.com>
2020-08-18 12:54:09 +0200

    Fix

--- setup_nightly.py
+++ setup_nightly.py
@@ -31,7 +31,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
-    packages=["pycaret-nightly"],
+    packages=["pycaret"],
     include_package_data=True,
     install_requires=required
 )
