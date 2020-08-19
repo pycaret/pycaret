f48195b4f772e025a4225064a2d3633bc156ebe3
Daiki Katsuragawa <50144563+daikikatsuragawa@users.noreply.github.com>
2020-08-20 00:06:56 +0900

    Update test_arules.py

--- pycaret/tests/test_arules.py
+++ pycaret/tests/test_arules.py
@@ -10,26 +10,37 @@ def test():
     # loading dataset
     data = pycaret.datasets.get_data("france")
     assert isinstance(data, pd.core.frame.DataFrame)
+    row, col = data.shape
+    assert row ==  8557
+    assert col == 8
 
     # init setup
     arul101 = pycaret.arules.setup(data = data, transaction_id = "InvoiceNo", item_id = "Description", session_id = 123)
     assert isinstance(arul101, tuple)
     assert isinstance(arul101[0], pd.core.frame.DataFrame)
+    row, col = arul101[0].shape
+    assert row ==  8557
+    assert col == 8
     assert isinstance(arul101[1], str)
     assert isinstance(arul101[2], str)
     assert isinstance(arul101[4], int)
     assert isinstance(arul101[5], list)
-
+  
     # create model
     model = pycaret.arules.create_model()
     assert isinstance(model, pd.core.frame.DataFrame)
+    row, col = model.shape
+    assert row == 141
+    assert col == 9
 
     # get rules
     rules = pycaret.arules.get_rules(data = data, transaction_id = "InvoiceNo", item_id = "Description")
     assert isinstance(rules, pd.core.frame.DataFrame)
+    row, col = rules.shape
+    assert row == 141
+    assert col == 9
 
     assert 1 == 1
 
 if __name__ == "__main__":
     test()
-    
