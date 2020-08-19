c64e925d46d42f8ef8b52d5d180fce9e67fcfc6a
Daiki Katsuragawa <50144563+daikikatsuragawa@users.noreply.github.com>
2020-08-19 23:50:22 +0900

    Update test_clustering.py (#485)

--- pycaret/tests/test_clustering.py
+++ pycaret/tests/test_clustering.py
@@ -1,6 +1,7 @@
 import os, sys
 sys.path.insert(0, os.path.abspath(".."))
 
+import pandas as pd
 import pytest
 import pycaret.clustering
 import pycaret.datasets
@@ -8,10 +9,12 @@ import pycaret.datasets
 def test():
     # loading dataset
     data = pycaret.datasets.get_data('jewellery')
+    assert isinstance(data, pd.core.frame.DataFrame)
 
     # init setup
     clu1 = pycaret.clustering.setup(data, normalize = True, silent=True, html=False, session_id=123)
-
+    assert isinstance(clu1, tuple)
+    
     # create model
     kmeans = pycaret.clustering.create_model('kmeans')
     kmodes = pycaret.clustering.create_model('kmodes')
@@ -19,6 +22,8 @@ def test():
     # assign model
     kmeans_results = pycaret.clustering.assign_model(kmeans)
     kmodes_results = pycaret.clustering.assign_model(kmodes)
+    assert isinstance(kmeans_results, pd.core.frame.DataFrame)
+    assert isinstance(kmodes_results, pd.core.frame.DataFrame)
 
     # save model
     pycaret.clustering.save_model(kmeans, 'kmeans_model_23122019')
@@ -28,16 +33,22 @@ def test():
     
     # predict model
     kmeans_predictions = pycaret.clustering.predict_model(model = kmeans, data = data)
+    assert isinstance(kmeans_predictions, pd.core.frame.DataFrame)
 
     # returns table of models
     all_models = pycaret.clustering.models()
+    assert isinstance(all_models, pd.core.frame.DataFrame)
     
     # get config
     X = pycaret.clustering.get_config('X')
     seed = pycaret.clustering.get_config('seed')
+    assert isinstance(X, pd.core.frame.DataFrame)
+    assert isinstance(seed, int)
     
     # set config
-    pycaret.clustering.set_config('seed', 123) 
+    pycaret.clustering.set_config('seed', 124)
+    seed = pycaret.clustering.get_config('seed')
+    assert seed == 124
 
     assert 1 == 1
     
