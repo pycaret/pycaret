458faac874df92aba68a82b2efe84274de4345a6
clgarciga <clgarciga@users.noreply.github.com>
2020-08-21 15:16:42 -0400

    Exampand test_preprocess to include all datasets in pycaret.

--- pycaret/tests/test_preprocess.py
+++ pycaret/tests/test_preprocess.py
@@ -1,4 +1,5 @@
 import os, sys
+
 sys.path.insert(0, os.path.abspath(".."))
 
 import pandas as pd
@@ -6,20 +7,28 @@ import pytest
 import pycaret.datasets
 import pycaret.preprocess
 
+
 def test():
-    # loading dataset
-    data = pycaret.datasets.get_data("juice")
-    target = "Purchase"
+    index = pd.read_csv("datasets/index.csv")
 
-    # preprocess all in one
-    X = pycaret.preprocess.Preprocess_Path_One(train_data=data, target_variable=target, display_types=False)
-    assert isinstance(X, pd.core.frame.DataFrame)
+    for _, row in index.iterrows():
 
-    # preprocess all in one unsupervised
-    X = pycaret.preprocess.Preprocess_Path_Two(train_data=data, display_types=False)
-    assert isinstance(X, pd.core.frame.DataFrame)
+        # loading dataset
+        data = pycaret.datasets.get_data(row['Dataset'])
+
+        if row['Target Variable 1']!='None':
+            X = pycaret.preprocess.Preprocess_Path_One(train_data=data, target_variable=row['Target Variable 1'], display_types=False)
+            assert isinstance(X, pd.core.frame.DataFrame)
+        if row['Target Variable 2']!='None':
+            X = pycaret.preprocess.Preprocess_Path_One(train_data=data, target_variable=row['Target Variable 2'], display_types=False)
+            assert isinstance(X, pd.core.frame.DataFrame)
+
+        # preprocess all in one unsupervised
+        X = pycaret.preprocess.Preprocess_Path_Two(train_data=data, display_types=False)
+        assert isinstance(X, pd.core.frame.DataFrame)
 
     assert 1 == 1
-    
+
+
 if __name__ == "__main__":
     test()
