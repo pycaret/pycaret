e6cc9dfcdf268717aee79567afd9fe23c86bf9a5
Antoni Baum <antoni.baum@protonmail.com>
2020-10-11 23:11:31 +0200

    Fix

--- pycaret/containers/models/regression.py
+++ pycaret/containers/models/regression.py
@@ -1619,25 +1619,12 @@ class CatBoostRegressorContainer(RegressorContainer):
         tune_grid = {
             "depth": list(range(1, 12)),
             "n_estimators": np_list_arange(10, 300, 10, inclusive=True),
-            "bagging_temperature": [
-                0,
-                0.01,
-                0.02,
-                0.03,
-                0.05,
-                0.09,
-                0.15,
-                0.25,
-                0.5,
-                0.75,
-            ],
             "random_strength": np_list_arange(0, 0.8, 0.1, inclusive=True),
             "l2_leaf_reg": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 50, 100, 200],
         }
         tune_distributions = {
             "depth": IntUniformDistribution(1, 11),
             "n_estimators": IntUniformDistribution(10, 300),
-            "bagging_temperature": UniformDistribution(0, 0.75, log=True),
             "random_strength": UniformDistribution(0, 0.8),
             "l2_leaf_reg": IntUniformDistribution(1, 200, log=True),
         }
