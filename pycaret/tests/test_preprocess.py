b8ff871157d5f751c68a5aa739e84a76516ea495
Antoni Baum <antoni.baum@protonmail.com>
2020-10-16 00:09:52 +0200

    Move preprocess

--- pycaret/tests/test_preprocess.py
+++ pycaret/tests/test_preprocess.py
@@ -4,7 +4,7 @@ sys.path.insert(0, os.path.abspath(".."))
 import pandas as pd
 import pytest
 import pycaret.datasets
-import pycaret.preprocess
+import pycaret.internal.preprocess
 
 def test():
     # loading dataset
@@ -12,7 +12,7 @@ def test():
     target = "Purchase"
 
     # preprocess all in one
-    pipe = pycaret.preprocess.Preprocess_Path_One(train_data=data, target_variable=target, display_types=False)
+    pipe = pycaret.internal.preprocess.Preprocess_Path_One(train_data=data, target_variable=target, display_types=False)
     X = pipe.fit_transform(data)
     assert isinstance(X, pd.core.frame.DataFrame)
 
