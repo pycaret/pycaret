e0e30ebce5c9b3d32c279c7f77baea885f9be902
Antoni Baum <antoni.baum@protonmail.com>
2020-12-09 16:05:00 +0100

    Revert ml_usecase change

--- pycaret/internal/tabular.py
+++ pycaret/internal/tabular.py
@@ -1097,9 +1097,9 @@ def setup(
 
     prep_pipe = pycaret.internal.preprocess.Preprocess_Path_One(
         train_data=train_data,
-        ml_usecase="classification"
-        if _ml_usecase == MLUsecase.CLASSIFICATION
-        else "regression",
+        #ml_usecase="classification"
+        #if _ml_usecase == MLUsecase.CLASSIFICATION
+        #else "regression",
         imputation_type=imputation_type,
         target_variable=target,
         imputation_regressor=imputation_regressor,
