7c322d0e802ec2423f08e6e0045f38df6b1a7d12
TremaMiguel <armando.trejo.marrufo@gmail.com>
2021-02-06 21:06:28 -0600

    functional tests time series metrics

--- pycaret/tests/test_utils.py
+++ pycaret/tests/test_utils.py
@@ -3,6 +3,7 @@ import os, sys
 sys.path.insert(0, os.path.abspath(".."))
 
 import numpy as np
+import pandas as pd
 import pytest
 import pycaret.utils
 import pycaret.classification
@@ -129,6 +130,41 @@ def test():
     # Ensure metric is rounded to default value
     mape = pycaret.utils.check_metric(actual, prediction, "MAPE")
     assert mape == 0.0469
+
+    # preparation (timeseries)
+    data = pycaret.datasets.get_data("airline", verbose=False)
+    train, test = sklearn.model_selection.train_test_split(
+        data, train_size=0.8, random_state=1, shuffle=False
+    )
+
+    # TO DO: call setup, create_model, finalize_model and predict_model
+    prediction = pd.Series([100]*len(test), index=test.index)
+    actual = test
+
+    # check metric(timeseries)
+    smape = pycaret.utils.check_metric(actual, prediction, "SMAPE")
+    assert isinstance(smape, float)
+    assert smape >= 0
+    mape = pycaret.utils.check_metric(actual, prediction, "MAPE_ts")
+    assert isinstance(mape, float)
+    assert mape >= 0
+    mase = pycaret.utils.check_metric(test, prediction, "MASE", train)
+    assert isinstance(mase, float)
+    assert mase >= 0
+    mae = pycaret.utils.check_metric(actual, prediction, "MAE_ts")
+    assert isinstance(mae, float)
+    assert mae >= 0
+    rmse = pycaret.utils.check_metric(actual, prediction, "RMSE_ts")
+    assert isinstance(rmse, float)
+    assert rmse >= 0
+
+    # Ensure metric is rounded to 2 decimals
+    smape = pycaret.utils.check_metric(actual, prediction, "SMAPE", 2)
+    assert smape == 1.24
+
+    # Ensure metric is rounded to default value
+    smape = pycaret.utils.check_metric(actual, prediction, "SMAPE")
+    assert smape == 1.2448
 
     # Metric does not exist
     with pytest.raises(ValueError, match="Couldn't find metric"):
