30483212701317bfb747b89b755451896563c843
Nikhil Gupta <mywork.ng@gmail.com>
2021-05-16 09:05:22 -0500

    fixed tests for latest code

--- pycaret/tests/test_time_series_splitters.py
+++ pycaret/tests/test_time_series_splitters.py
@@ -3,18 +3,20 @@ import pytest
 from random import choice, randint
 import numpy as np
 
+
 @pytest.fixture(scope="session", name="load_data")
 def load_data():
     """Load Pycaret Airline dataset."""
     from pycaret.datasets import get_data
+
     airline_data = get_data("airline")
     return airline_data
 
 
 parametrize_list = [
-    (randint(2, 5), randint(5, 10), "expandingwindow"),
-    (randint(2, 5), randint(5, 10), "slidingwindow"),
-    # (randint(2, 5), randint(5, 10), "timeseries"),  # Not supporting right now
+    (randint(2, 5), randint(5, 10), "expanding"),
+    (randint(2, 5), randint(5, 10), "rolling"),
+    (randint(2, 5), randint(5, 10), "sliding"),
 ]
 
 
@@ -26,36 +28,32 @@ def test_setup_initialization(fold, fh, fold_strategy, load_data):
         ExpandingWindowSplitter,
         SlidingWindowSplitter,
     )
-    from sklearn.model_selection import TimeSeriesSplit
 
-    #train_size = 0.7
     exp_name = setup(
         data=load_data,
         fold=fold,
         fh=fh,
         fold_strategy=fold_strategy,
-        #train_size=train_size
     )
 
-    if (fold_strategy == "expandingwindow") or (fold_strategy == "slidingwindow"):
-        if fold_strategy == "expandingwindow":
+    allowed_fold_strategies = ["expanding", "rolling", "sliding"]
+    if fold_strategy in allowed_fold_strategies:
+        if (fold_strategy == "expanding") or (fold_strategy == "rolling"):
             assert isinstance(exp_name.fold_generator, ExpandingWindowSplitter)
-        elif fold_strategy == "slidingwindow":
+        elif fold_strategy == "sliding":
             assert isinstance(exp_name.fold_generator, SlidingWindowSplitter)
 
-        #expected = int(len(load_data)*train_size) - fold * fh  # Since fh is an int, if train_size splits original data
-        expected = int(len(load_data) - fh) - fold * fh # if fh splits original data
+        # expected = int(len(load_data)*train_size) - fold * fh  # Since fh is an int, if train_size splits original data
+        expected = int(len(load_data) - fh) - fold * fh  # if fh splits original data
         assert exp_name.fold_generator.initial_window == expected
-        assert np.all(exp_name.fold_generator.fh == np.arange(1, fh+1))
+        assert np.all(exp_name.fold_generator.fh == np.arange(1, fh + 1))
         assert exp_name.fold_generator.step_length == fh  # Since fh is an int
-    
-    # elif fold_strategy == "timeseries":
-    #     assert isinstance(exp_name.fold_generator, TimeSeriesSplit)
 
 
 setup_raises_list = [
-    (randint(50, 100), randint(10, 20), "expandingwindow"),
-    (randint(50, 100), randint(10, 20), "slidingwindow"),
+    (randint(50, 100), randint(10, 20), "expanding"),
+    (randint(50, 100), randint(10, 20), "rolling"),
+    (randint(50, 100), randint(10, 20), "sliding"),
 ]
 
 
