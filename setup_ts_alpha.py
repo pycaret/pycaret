f094abb5fa52c33bcdf7ccfa45710a1bbce284bd
Antoni Baum <antoni.baum@protonmail.com>
2021-06-26 23:30:38 +0200

    ts alpha setup

---
+++ setup_ts_alpha.py
@@ -0,0 +1,44 @@
+# Copyright (C) 2019-2020 Moez Ali <moez.ali@queensu.ca>
+# License: MIT, moez.ali@queensu.ca
+
+import time
+from setuptools import setup, find_packages
+
+nightly_version = "3.0.0"
+
+def readme():
+    with open("README.md") as f:
+        README = f.read()
+    return README
+
+
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+with open("requirements-ts.txt") as f:
+    required += f.read().splitlines()
+
+with open("requirements-optional.txt") as f:
+    optional_required = f.read().splitlines()
+
+setup(
+    name="pycaret-ts-alpha",
+    version=str(nightly_version) + ".dev" + str(int(time.time())),
+    description="PyCaret - An open source, low-code machine learning library in Python.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/pycaret/pycaret",
+    author="Moez Ali",
+    author_email="moez.ali@queensu.ca",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    include_package_data=True,
+    install_requires=required,
+    extras_require={"full": optional_required,},
+)
