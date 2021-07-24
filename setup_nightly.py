0d31ae54f6da425ca9f84334067edf79e34c1f4c
e97371 <wen-yang.ku@aviva.com>
2021-07-24 13:59:32 -0400

    add optional_required library to test

--- setup_nightly.py
+++ setup_nightly.py
@@ -44,5 +44,5 @@ setup(
     include_package_data=True,
     install_requires=required,
     extras_require={"full": optional_required,
-                    "test": test_required},
+                    "test": test_required + optional_required},
 )
