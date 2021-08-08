707f2f4085c1c134d97c7ada2f3f5e53ee690c13
Nikhil Gupta <mywork.ng@gmail.com>
2021-08-08 08:05:29 -0500

    initial attempt at adding test - more work TBD

--- pycaret/internal/pycaret_experiment/time_series_experiment.py
+++ pycaret/internal/pycaret_experiment/time_series_experiment.py
@@ -1,7 +1,13 @@
+from pandas.core.frame import DataFrame
 from sktime.forecasting.model_selection import (
     ExpandingWindowSplitter,
     SlidingWindowSplitter,
 )
+
+import statsmodels.api as sm
+from statsmodels.tsa.seasonal import seasonal_decompose, STL
+from statsmodels.tsa.stattools import acf, pacf
+
 
 from pycaret.internal.pycaret_experiment.utils import highlight_setup, MLUsecase
 from pycaret.internal.pycaret_experiment.supervised_experiment import (
@@ -51,6 +57,7 @@ from joblib import Parallel, delayed  # type: ignore
 from sktime.utils.validation.forecasting import check_y_X  # type: ignore
 from sktime.forecasting.model_selection import SlidingWindowSplitter  # type: ignore
 
+import pmdarima as pm
 
 warnings.filterwarnings("ignore")
 LOGGER = get_logger()
@@ -2286,6 +2293,9 @@ class TimeSeriesExperiment(_SupervisedExperiment):
             verbose=verbose,
         )
 
+    def test_model(self, estimator, test: str = "residuals",) -> pd.DataFrame:
+        pass
+
     def plot_model(
         self,
         estimator,
@@ -3265,3 +3275,295 @@ class TimeSeriesExperiment(_SupervisedExperiment):
                     start_with_window=True,
                 )
         return fold_generator
+
+
+###################### TESTS STARTS HERE ####################
+
+
+def test_(data, test: str, *kwargs):
+    pass
+
+
+def test_all(data, alpha=0.05, verbose=True):
+    result_summary_stats = test_summary_statistics(data=data)
+    result_summary_stats["Setting"] = ""
+    result_summary_stats.set_index("Setting", append=True, inplace=True)
+
+    result_wn = test_is_white_noise(data, verbose=verbose)
+    result_adf = test_is_stationary_adf(data, verbose=verbose)
+    result_kpss = test_is_stationary_kpss(data, verbose=verbose)
+    result_normality = test_is_gaussian(data, verbose=verbose)
+
+    all_dfs = [
+        result_summary_stats,
+        result_wn[1],
+        result_adf[1],
+        result_kpss[1],
+        result_normality[1],
+    ]
+    final = pd.concat(all_dfs)
+    return final
+
+
+def test_summary_statistics(data: pd.Series):
+    distinct_counts = dict(data.value_counts(normalize=True))
+    result = {
+        "Length": len(data),
+        "Mean": data.mean(),
+        "Median": data.median(),
+        "Standard Deviation": data.std(),
+        "Variance": data.var(),
+        "Kurtosis": data.kurt(),
+        "Skewness": data.skew(),
+        "# Distinct Values": len(distinct_counts),
+    }
+
+    result = pd.DataFrame(result, index=["value"]).T.reset_index()
+    result = _format_test_results(result, "Summary", "Statistics")
+
+    return result
+
+
+def test_is_white_noise(
+    data: pd.Series, lags: List[int] = [24, 48], verbose: bool = False
+):
+    """Performs the Ljung-Box test for testing if a time series is White Noise
+
+    H0: The data is consistent with white noise
+    Ha: The data is not consistent with white noise.
+
+    Parameters
+    ----------
+    data : pd.Series
+        The time series that has to be tested
+    lags : List[int], optional
+        The lags used to test the autocorelation for white noise, by default [24, 48]
+    """
+    results = sm.stats.acorr_ljungbox(data, lags=lags, return_df=True)
+    results["White Noise"] = results["lb_pvalue"] > 0.05
+    is_white_noise = False if results["White Noise"].all() == False else True
+    results.rename(
+        columns={"lb_stat": "LB Test Statictic", "lb_pvalue": "LB p-value"},
+        inplace=True,
+    )
+
+    results.reset_index(inplace=True)
+    results.rename(columns={"index": "Setting"}, inplace=True)
+    results = pd.melt(results, id_vars="Setting", var_name="index", value_name="value")
+    results = _format_test_results(results, "White Noise", "Ljung-Box")
+
+    if verbose:
+        return is_white_noise, results
+    else:
+        return is_white_noise
+
+
+def test_is_stationary(data):
+    pass
+
+
+def test_is_stationary_adf(data: pd.Series, alpha: float = 0.05, verbose: bool = False):
+    """Checks Difference Stationarity
+
+    H0: The time series is not stationary (has a unit root)
+    H1: The time series is stationary
+
+    If not stationary, try differencing
+
+    Parameters
+    ----------
+    data : pd.Series
+        Time Series to be tested
+    """
+    from statsmodels.tsa.stattools import adfuller
+
+    results = adfuller(data, autolag="AIC", maxlag=20)
+    test_statistic = results[0]
+    critical_values = results[4]
+    p_value = results[1]
+    stationarity = True if p_value < alpha else False
+    details = {
+        "Stationarity": stationarity,
+        "p-value": p_value,
+        "Test Statistic": test_statistic,
+    }
+    critical_values = {
+        f"Critical Value {key}": value for key, value in critical_values.items()
+    }
+    details.update(critical_values)
+
+    details = pd.DataFrame(details, index=["value"]).T.reset_index()
+    details["Setting"] = alpha
+    details = _format_test_results(details, "Stationarity", "ADF")
+
+    if verbose:
+        return stationarity, details
+    else:
+        return stationarity
+
+
+def test_is_stationary_kpss(
+    data: pd.Series, alpha: float = 0.05, verbose: bool = False
+):
+    """Checks Stationarity around a deterministic trend
+
+    H0: The time series is trend stationary
+    H1: The time series is not trend stationary
+
+    If not trend stationary, then try removing trend.
+
+    Parameters
+    ----------
+    data : pd.Series
+        Time Series to be tested
+    """
+    from statsmodels.tsa.api import kpss
+
+    results = kpss(data, regression="ct", nlags="auto")
+    test_statistic = results[0]
+    p_value = results[1]
+    critical_values = results[3]
+    stationarity = False if p_value < alpha else True
+    details = {
+        "Trend Stationarity": stationarity,
+        "p-value": p_value,
+        "Test Statistic": test_statistic,
+    }
+    critical_values = {
+        f"Critical Value {key}": value for key, value in critical_values.items()
+    }
+    details.update(critical_values)
+
+    details = pd.DataFrame(details, index=["value"]).T.reset_index()
+    details["Setting"] = alpha
+    details = _format_test_results(details, "Stationarity", "KPSS")
+
+    if verbose:
+        return stationarity, details
+    else:
+        return stationarity
+
+
+def test_trend():
+    pass
+
+
+def test_seasonality():
+    pass
+
+
+def test_is_gaussian(data: pd.Series, alpha: float = 0.05, verbose: bool = False):
+    """Performs the shapiro test to check for normality of data
+    """
+    from scipy.stats import shapiro, norm
+
+    p_value = shapiro(data.values.squeeze())[1]
+    normality = True if p_value > alpha else False
+    details = {
+        "Normality": normality,
+        "p-value": p_value,
+    }
+    details = pd.DataFrame(details, index=["value"]).T.reset_index()
+    details["Setting"] = alpha
+    details = _format_test_results(details, "Normality", "Shapiro")
+
+    if verbose:
+        return normality, details
+    else:
+        return normality
+
+
+def recommend_d(data: pd.Series, **kwargs) -> int:
+    """Returns the recommended value of differencing order 'd' to use
+
+    Parameters
+    ----------
+    data : pd.Series
+        The data for which the differencing order needs to be calculated
+
+    *kwargs: Keyword arguments that can be passed to the difference test.
+        Values are:
+            alpha : float, optional
+                Significance Value, by default 0.05
+            test : str, optional
+                The test to use to test the order of differencing, by default 'kpss'
+            max_d : int, optional
+                maximum differencing order to try, by default 2
+
+    Returns
+    -------
+    int
+        The differencing order to use
+    """
+    recommended_d = pm.arima.ndiffs(data, **kwargs)
+    return recommended_d
+
+
+def recommend_D(data: pd.Series, sp: int, **kwargs) -> int:
+    """Returns the recommended value of differencing order 'd' to use
+
+    Parameters
+    ----------
+    data : pd.Series
+        The data for which the differencing order needs to be calculated
+
+    sp : int
+        The number of seasonal periods (i.e., frequency of the time series)
+
+    *kwargs: Keyword arguments that can be passed to the difference test.
+        Values are:
+            alpha : float, optional
+                Significance Value, by default 0.05
+            test : str, optional
+                Type of unit root test of seasonality to use in order to detect
+                seasonal periodicity. Valid tests include (“ocsb”, “ch”).
+                Note that the CHTest is very slow for large data.
+            max_D : int, optional
+                maximum seasonal differencing order to try, by default 2
+
+    Returns
+    -------
+    int
+        The differencing order to use
+    """
+    recommended_D = pm.arima.ndsiffs(data, m=sp, **kwargs)
+    return recommended_D
+
+
+def _format_test_results(
+    result: pd.DataFrame, test: str, test_name: str
+) -> pd.DataFrame:
+    """Formats the results of a test into a multiindex dataframe with first
+    level = "test" and second level = "name". The final dataframe has a column
+    called value corresponding to the "name"
+
+    Parameters
+    ----------
+    result : pd.DataFrame
+        The unformatted result dataframe that has to be formatted.
+        Must contain
+          - a column called "index" corresponding to the
+            values (test statistic) to be used for the second level.
+          - a column called "value" which contains the
+            value of the test statistic.
+        Optionally
+          - a column called "Setting" which can contain information
+            such as alpha value used, or number of lags used
+
+    test_name : str
+        The name of the test. Used as the value for the first level
+
+    Returns
+    -------
+    pd.DataFrame
+        [description]
+    """
+    result.rename(columns={"index": "Property"}, inplace=True)
+    result["Test"] = test
+    result["Test Name"] = test_name
+    if "Setting" in result.columns:
+        result.set_index(["Test", "Test Name", "Property", "Setting"], inplace=True)
+    else:
+        result.set_index(["Test", "Test Name", "Property"], inplace=True)
+    return result
+
