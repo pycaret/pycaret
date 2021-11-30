d1c5d94946082232b76446a4c33ad2c2c2b3bb27
Nikhil Gupta <mywork.ng@gmail.com>
2021-11-29 18:18:04 -0600

    updates per #1900

--- pycaret/tests/test_time_series_metrics.py
+++ pycaret/tests/test_time_series_metrics.py
@@ -5,7 +5,7 @@ import pytest
 import numpy as np  # type: ignore
 import pandas as pd  # type: ignore
 
-from pycaret.containers.metrics.time_series import _inpi_loss
+from pycaret.containers.metrics.time_series import inpi
 
 
 pytestmark = pytest.mark.filterwarnings("ignore::UserWarning")
@@ -29,22 +29,22 @@ def test_inpi_loss():
 
     # All pass
     y_true = pd.Series([1, 2, 3, 4])
-    loss = _inpi_loss(y_true=y_true, y_pred=y_pred, lower=lower, upper=upper)
+    loss = inpi(y_true=y_true, y_pred=y_pred, lower=lower, upper=upper)
     assert loss == 1.00
 
     # Upper Limit breached
     y_true = pd.Series([1, 2, 4, 4])
-    loss = _inpi_loss(y_true=y_true, y_pred=y_pred, lower=lower, upper=upper)
+    loss = inpi(y_true=y_true, y_pred=y_pred, lower=lower, upper=upper)
     assert loss == 0.75
 
     # Lower Limit breached
     y_true = pd.Series([1, 1, 3, 4])
-    loss = _inpi_loss(y_true=y_true, y_pred=y_pred, lower=lower, upper=upper)
+    loss = inpi(y_true=y_true, y_pred=y_pred, lower=lower, upper=upper)
     assert loss == 0.75
 
     # Both Limits breached
     y_true = pd.Series([1, 1, 4, 4])
-    loss = _inpi_loss(y_true=y_true, y_pred=y_pred, lower=lower, upper=upper)
+    loss = inpi(y_true=y_true, y_pred=y_pred, lower=lower, upper=upper)
     assert loss == 0.50
 
     ##################################
@@ -53,7 +53,7 @@ def test_inpi_loss():
     lower = pd.Series([np.nan] * 4)
     upper = pd.Series([np.nan] * 4)
     y_true = pd.Series([1, 2, 3, 4])
-    loss = _inpi_loss(y_true=y_true, y_pred=y_pred, lower=lower, upper=upper)
+    loss = inpi(y_true=y_true, y_pred=y_pred, lower=lower, upper=upper)
     assert loss is np.nan
 
     ##################################
@@ -62,7 +62,7 @@ def test_inpi_loss():
     lower = pd.Series([0.5, 1.5, 2.5, 3.5])
     upper = pd.Series([1.5, 2.5, 3.5, 4.5])
     y_true = pd.Series([1, 2, np.nan, 4])
-    loss = _inpi_loss(y_true=y_true, y_pred=y_pred, lower=lower, upper=upper)
+    loss = inpi(y_true=y_true, y_pred=y_pred, lower=lower, upper=upper)
     assert loss is np.nan
 
     ######################################
@@ -71,5 +71,5 @@ def test_inpi_loss():
     lower = pd.Series([0.5, 1.5, 2.5, 3.5], index=[0, 1, 2, 3])
     upper = pd.Series([1.5, 2.5, 3.5, 4.5], index=[0, 1, 2, 3])
     y_true = pd.Series([1, 2, 3, 4], index=[0, 1, 2, 4])
-    loss = _inpi_loss(y_true=y_true, y_pred=y_pred, lower=lower, upper=upper)
+    loss = inpi(y_true=y_true, y_pred=y_pred, lower=lower, upper=upper)
     assert loss is np.nan
