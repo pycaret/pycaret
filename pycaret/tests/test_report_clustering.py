dfded94c1acac9597da65e7eebb4ddf01327fec5
Antoni Baum <antoni.baum@protonmail.com>
2021-12-04 17:34:19 +0000

    Fix test

--- pycaret/tests/test_report_clustering.py
+++ pycaret/tests/test_report_clustering.py
@@ -4,75 +4,67 @@ from pycaret.clustering import *
 
 sys.path.insert(0, os.path.abspath(".."))
 
-def setup():
-    dataset = get_data('mice')
+
+def _setup():
+    dataset = get_data("mice")
     data = dataset.sample(frac=0.95, random_state=786)
     data_unseen = dataset.drop(data.index)
     data.reset_index(drop=True, inplace=True)
     data_unseen.reset_index(drop=True, inplace=True)
-    exp_clu101 = setup(data, normalize=True,
-                       ignore_features=['MouseID'],
-                       session_id=123)
+    exp_clu101 = setup(
+        data, normalize=True, ignore_features=["MouseID"], session_id=123
+    )
 
 
 def test_report_kmeans():
-    setup()
-    model = create_model('kmeans')
+    _setup()
+    model = create_model("kmeans")
     create_report(model, "report_anomaly_kmeans", "html")
 
 
-def test_report_ap ():
-    setup()
-    model = create_model('ap')
+def test_report_ap():
+    _setup()
+    model = create_model("ap")
     create_report(model, "report_anomaly_ap", "html")
 
 
 def test_report_meanshift():
-    setup()
-    model = create_model('meanshift')
+    _setup()
+    model = create_model("meanshift")
     create_report(model, "report_anomaly_meanshift", "html")
 
 
 def test_report_sc():
-    setup()
-    model = create_model('sc')
+    _setup()
+    model = create_model("sc")
     create_report(model, "report_anomaly_sc", "html")
 
 
 def test_report_hclust():
-    setup()
-    model = create_model('hclust')
+    _setup()
+    model = create_model("hclust")
     create_report(model, "report_anomaly_hclust", "html")
 
 
 def test_report_dbscan():
-    setup()
-    model = create_model('dbscan')
+    _setup()
+    model = create_model("dbscan")
     create_report(model, "report_anomaly_dbscan", "html")
 
 
 def test_report_optics():
-    setup()
-    model = create_model('optics')
+    _setup()
+    model = create_model("optics")
     create_report(model, "report_anomaly_optics", "html")
 
 
 def test_report_birch():
-    setup()
-    model = create_model('birch')
+    _setup()
+    model = create_model("birch")
     create_report(model, "report_anomaly_birch", "html")
 
 
 def test_report_kmodes():
-    setup()
-    model = create_model('kmodes')
+    _setup()
+    model = create_model("kmodes")
     create_report(model, "report_anomaly_kmodes", "html")
-
-
-def test():
-    test_report_kmeans()
-    assert 1==1
-
-
-if __name__ == "__main__":
-    test()
--- pycaret/tests/test_report_nlp.py
+++ pycaret/tests/test_report_nlp.py
@@ -6,61 +6,70 @@ from pycaret.nlp import *
 sys.path.insert(0, os.path.abspath(".."))
 
 
-def setup():
-    data = get_data('kiva')
+def _setup():
+    data = get_data("kiva")
     # sampling the data to select only 1000 documents
     data = data.sample(1000, random_state=786).reset_index(drop=True)
 
-    exp_nlp102 = setup(data=data, target='en', session_id=123,
-                       custom_stopwords=['loan', 'income', 'usd', 'many', 'also', 'make', 'business', 'buy',
-                                         'sell', 'purchase', 'year', 'people', 'able', 'enable', 'old', 'woman',
-                                         'child', 'school'],
-                       log_experiment=True, experiment_name='kiva1')
+    exp_nlp102 = setup(
+        data=data,
+        target="en",
+        session_id=123,
+        custom_stopwords=[
+            "loan",
+            "income",
+            "usd",
+            "many",
+            "also",
+            "make",
+            "business",
+            "buy",
+            "sell",
+            "purchase",
+            "year",
+            "people",
+            "able",
+            "enable",
+            "old",
+            "woman",
+            "child",
+            "school",
+        ],
+        log_experiment=True,
+        experiment_name="kiva1",
+    )
 
 
 def test_report_lda():
-    setup()
+    _setup()
     #  Latent Dirichlet Allocation
-    model = create_model('lda')
-    create_report(model,"report_nlp_lds","html")
+    model = create_model("lda")
+    create_report(model, "report_nlp_lds", "html")
 
 
 def test_report_lsi():
-    setup()
+    _setup()
     # Latent Semantic Indexing
-    model = create_model('lsi')
+    model = create_model("lsi")
     create_report(model, "report_nlp_lsi", "html")
 
 
 def test_report_hdp():
-    setup()
+    _setup()
     # Hierarchical Dirichlet Process
-    model = create_model('hdp')
+    model = create_model("hdp")
     create_report(model, "report_nlp_hdp", "html")
 
 
 def test_report_rp():
-    setup()
+    _setup()
     # Random Projections
-    model = create_model('rp')
+    model = create_model("rp")
     create_report(model, "report_nlp_rp", "html")
 
 
 def test_report_nmf():
-    setup()
+    _setup()
     # Non - Negative Matrix Factorization
-    model = create_model('nmf')
+    model = create_model("nmf")
     create_report(model, "report_nlp_nmf", "html")
-
-
-def test():
-    test_report_lda()
-    test_report_lsi()
-    test_report_hdp()
-    test_report_rp()
-    test_report_nmf()
-    assert 1==1
-
-
-if __name__ == "__main__":
-    test()
