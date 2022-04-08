e93bc7e5a211faf60ea5253c4623394340e3f016
Nikhil Gupta <mywork.ng@gmail.com>
2022-04-08 18:58:07 -0500

    fix for #2388

--- pycaret/time_series/forecasting/oop.py
+++ pycaret/time_series/forecasting/oop.py
@@ -440,7 +440,8 @@ class TSForecastingExperiment(_SupervisedExperiment, TSForecastingPreprocessor):
         ValueError
             Raised when
             (1) Index has duplicate values.
-            (2) Index is not one of the allowed types and seasonal period is not provided
+            (2) Data has missing index values.
+            (3) Index is not one of the allowed types and seasonal period is not provided
         """
 
         #### Set Index if necessary ----
@@ -464,7 +465,27 @@ class TSForecastingExperiment(_SupervisedExperiment, TSForecastingPreprocessor):
 
         #### Data must not have duplicate indices ----
         if len(self.data.index) != len(set(self.data.index)):
-            raise ValueError("Index may not have duplicate values!")
+            raise ValueError(
+                "Index may not have duplicate values! Please check and correct before passing to pycaret"
+            )
+
+        #### Data must not have missing indices ----
+        expected_idx = pd.period_range(min(self.data.index), max(self.data.index))
+        if len(self.data.index) != len(expected_idx):
+            missing_index = [
+                index for index in expected_idx if index not in self.data.index
+            ]
+            raise ValueError(
+                f"Missing Indices\n\n{missing_index}"
+                "\n\nData has missing indices!"
+                "\nMany models, plotting, and testing functionality does not work with missing indices."
+                "\nPlease add missing indices to data before passing to pycaret."
+                "\nYou can do that by using the following code snippet & then enabling imputation of missing values in `setup`"
+                "\n\n# Assuming `data` is a pandas dataframe"
+                "\n>>> import numpy as np"
+                "\n>>> idx = pd.period_range(min(data.index), max(data.index))"
+                "\n>>> data = data.reindex(idx, fill_value=np.nan)"
+            )
 
         #### Check Index Type ----
         allowed_freq_index_types = (pd.PeriodIndex, pd.DatetimeIndex)
@@ -1065,8 +1086,10 @@ class TSForecastingExperiment(_SupervisedExperiment, TSForecastingPreprocessor):
 
     def _check_setup_ran(self):
         if not self._setup_ran:
-            raise RuntimeError("This function requires the users to run setup() first.\
-                More info: https://pycaret.gitbook.io/docs/get-started/quickstart")
+            raise RuntimeError(
+                "This function requires the users to run setup() first."
+                "\nMore info: https://pycaret.gitbook.io/docs/get-started/quickstart"
+            )
 
     def setup(
         self,
