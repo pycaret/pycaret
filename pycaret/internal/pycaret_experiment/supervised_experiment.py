9f30747fac6743f63b43469c99eb6a46d08a50a3
Antoni Baum <antoni.baum@protonmail.com>
2022-09-04 02:33:50 +0200

    Fix bad import

--- pycaret/internal/pycaret_experiment/supervised_experiment.py
+++ pycaret/internal/pycaret_experiment/supervised_experiment.py
@@ -4,7 +4,6 @@ import os
 import time
 import traceback
 import warnings
-from collections import Iterable
 from copy import copy
 from functools import partial
 from typing import Any, Dict, List, Optional, Set, Tuple, Union
