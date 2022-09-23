2057a626a3b039d8e7eaa5fcda3e5818bf2d5ba1
Antoni Baum <antoni.baum@protonmail.com>
2022-09-23 21:44:52 +0000

    Update version to rc4
    Signed-off-by: Antoni Baum <antoni.baum@protonmail.com>

--- pycaret/utils/__init__.py
+++ pycaret/utils/__init__.py
@@ -1,6 +1,6 @@
 from sklearn.metrics._scorer import _PredictScorer, get_scorer  # type: ignore
 
-version_ = "3.0.0.rc3"
+version_ = "3.0.0.rc4"
 nightly_version_ = "3.0.0"
 
 __version__ = version_
