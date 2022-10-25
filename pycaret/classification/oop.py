b9830ad6ab24c11e34580254de72ccd932fb1443
tvdboom <m.524687@gmail.com>
2022-10-25 09:45:24 +0200

    fix import and train test split bugs

--- pycaret/classification/oop.py
+++ pycaret/classification/oop.py
@@ -142,7 +142,7 @@ class ClassificationExperiment(_SupervisedExperiment, Preprocessor):
         rare_value: str = "rare",
         polynomial_features: bool = False,
         polynomial_degree: int = 2,
-        low_variance_threshold: Optional[float] = 0,
+        low_variance_threshold: Optional[float] = None,
         group_features: Optional[list] = None,
         group_names: Optional[Union[str, list]] = None,
         remove_multicollinearity: bool = False,
