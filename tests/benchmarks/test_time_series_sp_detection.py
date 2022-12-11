a310a817756a069a3b3d996df36190e1e20b9bbf
Nikhil Gupta <mywork.ng@gmail.com>
2022-12-11 15:04:30 -0600

    added initial benchmark for harmonic filtering

--- tests/benchmarks/test_time_series_sp_detection.py
+++ tests/benchmarks/test_time_series_sp_detection.py
@@ -10,20 +10,70 @@ def test_benchmark_sp_to_use_using_auto():
 
     properties = get_data("index", folder="time_series/seasonal", verbose=False)
     properties[["index", "s"]]
-    detected_sp = []
+    candidate_sps = []
+
+    sig_sps = []
+    all_sps = []
+    primary_sp = []
+
+    sig_sps_no_harmonics = []
+    all_sp_no_harmonics = []
+    primary_sp_no_harmonics = []
 
     exp = TSForecastingExperiment()
     for _, (index, _) in enumerate(properties[["index", "s"]].values):
         y = get_data(index, folder="time_series/seasonal", verbose=False)
         exp.setup(data=y, session_id=index)
-        detected_sp.append(exp.primary_sp_to_use)
-    properties["detected_sp"] = detected_sp
-    properties["equal"] = properties["s"] == properties["detected_sp"]
-    properties["multiple"] = properties["detected_sp"] % properties["s"] == 0
+        candidate_sps.append(exp.candidate_sps)
+
+        sig_sps.append(exp.significant_sps)
+        all_sps.append(exp.all_sps_to_use)
+        primary_sp.append(exp.primary_sp_to_use)
+
+        sig_sps_no_harmonics.append(exp.significant_sps_no_harmonics)
+        all_sp_no_harmonics.append(
+            exp.significant_sps_no_harmonics[0 : len(exp.all_sps_to_use)]
+        )
+        primary_sp_no_harmonics.append(exp.significant_sps_no_harmonics[0])
+
+    properties["candidate_sps"] = candidate_sps
+
+    # 1.0 Test with harmonics included ----
+
+    properties["sig_sps"] = sig_sps
+    properties["all_sps"] = all_sps
+    properties["primary_sp"] = primary_sp
+
+    properties["equal"] = properties["s"] == properties["primary_sp"]
+    properties["multiple"] = properties["primary_sp"] % properties["s"] == 0
 
     per_correct = len(properties.query("equal == True")) / len(properties)
     per_correct_multiple = len(properties.query("multiple == True")) / len(properties)
 
     # Current benchmark to beat ----
-    assert per_correct > 0.9211  # 0.8076
-    assert per_correct_multiple > 0.9307  # 0.8173
+    assert per_correct > 0.9211
+    assert per_correct_multiple > 0.9307
+
+    # 2.0 Test with harmonics excluded ----
+
+    properties["sig_sps_no_harmonics"] = sig_sps_no_harmonics
+    properties["all_sp_no_harmonics"] = all_sp_no_harmonics
+    properties["primary_sp_no_harmonics"] = primary_sp_no_harmonics
+
+    properties["equal_no_harmonics"] = (
+        properties["s"] == properties["primary_sp_no_harmonics"]
+    )
+    properties["multiple_no_harmonics"] = (
+        properties["primary_sp_no_harmonics"] % properties["s"] == 0
+    )
+
+    per_correct_no_harmonics = len(
+        properties.query("equal_no_harmonics == True")
+    ) / len(properties)
+    per_correct_multiple_no_harmonics = len(
+        properties.query("multiple_no_harmonics == True")
+    ) / len(properties)
+
+    # Current benchmark to beat ----
+    assert per_correct_no_harmonics > 0.1192
+    assert per_correct_multiple_no_harmonics > 0.8153
