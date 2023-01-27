e9c25d6c5416b51272e8816aa6e246b170614a27
Antoni Baum <antoni.baum@protonmail.com>
2023-01-27 02:23:16 +0000

    Use joblib fork
    Signed-off-by: Antoni Baum <antoni.baum@protonmail.com>

--- pycaret/__init__.py
+++ pycaret/__init__.py
@@ -1,14 +1,4 @@
-import sys
-
 from pycaret.utils import __version__
 from pycaret.utils._show_versions import show_versions
 
 __all__ = ["show_versions", "__version__"]
-
-# Fix for joblib on 3.7
-# Otherwise, it will default to cloudpickle which
-# uses pickle5, causing exceptions with joblib
-if sys.version_info < (3, 8):
-    from joblib.externals.loky.backend.reduction import set_loky_pickler
-
-    set_loky_pickler("pickle")
