b47a896ed8725210ff97c874e684e0366da4d38f
Antoni Baum <antoni.baum@protonmail.com>
2023-03-04 23:36:11 +0000

    Redirect C-level and child stdout/stderr correctly
    Signed-off-by: Antoni Baum <antoni.baum@protonmail.com>

--- pycaret/internal/logging.py
+++ pycaret/internal/logging.py
@@ -9,25 +9,28 @@ import warnings
 from contextlib import redirect_stderr, redirect_stdout
 from typing import Callable, Optional, Union
 
+from wurlitzer import pipes
+
 
 # From https://stackoverflow.com/a/66209331
 class LoggerWriter:
     """Writer allowing redirection of streams to logger methods."""
 
-    def __init__(self, logfct: Callable):
-        self.logfct = logfct
-        self.buf = []
+    def __init__(self, writer: Callable):
+        self._writer = writer
+        self._msg = ""
 
-    def write(self, msg: str):
-        if msg.endswith("\n"):
-            self.buf.append(msg.rstrip("\n"))
-            self.logfct("".join(self.buf))
-            self.buf = []
-        else:
-            self.buf.append(msg)
+    def write(self, message: str):
+        self._msg = self._msg + message
+        while "\n" in self._msg:
+            pos = self._msg.find("\n")
+            self._writer(self._msg[:pos])
+            self._msg = self._msg[pos + 1 :]
 
     def flush(self):
-        pass
+        if self._msg != "":
+            self._writer(self._msg)
+            self._msg = ""
 
 
 class redirect_output:
@@ -35,16 +38,24 @@ class redirect_output:
 
     def __init__(self, logger: Optional[logging.Logger] = None):
         self.logger = logger or DummyLogger("dummy")
-        self.redirect_stdout = redirect_stdout(LoggerWriter(logger.info))
-        self.redirect_stderr = redirect_stderr(LoggerWriter(logger.warning))
+        # Current Python process redirects
+        self.redirect_stdout = redirect_stdout(LoggerWriter(self.logger.info))
+        self.redirect_stderr = redirect_stderr(LoggerWriter(self.logger.warning))
+        # This redirects stdout/stderr from C libraries and child processes (joblib)
+        self.c_redirect = pipes(
+            stdout=LoggerWriter(self.logger.info),
+            stderr=LoggerWriter(self.logger.warning),
+        )
 
     def __enter__(self):
         self.redirect_stdout.__enter__()
         self.redirect_stderr.__enter__()
+        self.c_redirect.__enter__()
 
     def __exit__(self, *args, **kwargs):
-        self.redirect_stdout.__exit__(*args, **kwargs)
+        self.c_redirect.__exit__(*args, **kwargs)
         self.redirect_stderr.__exit__(*args, **kwargs)
+        self.redirect_stdout.__exit__(*args, **kwargs)
 
 
 class DummyLogger(logging.Logger):
