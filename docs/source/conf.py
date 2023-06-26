324db1bfcf83aa1b93368aa6e1ef85ff37e2e410
Antoni Baum <antoni.baum@protonmail.com>
2023-06-26 02:12:04 +0000

    Bump version to 3.0.3
    Signed-off-by: Antoni Baum <antoni.baum@protonmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2020, Moez Ali"
 author = "Moez Ali"
 
 # The full version, including alpha/beta/rc tags
-release = "3.0.2"
+release = "3.0.3"
 
 
 # -- General configuration ---------------------------------------------------
