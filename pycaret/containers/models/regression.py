4f2f6bb0519e9a10bf5e78c3420b0970e9af13f3
CelestinoXP <celestinoxp@hotmail.com>
2023-07-27 09:17:56 +0100

    Prepare for XGBoost 2.0

--- pycaret/containers/models/regression.py
+++ pycaret/containers/models/regression.py
@@ -1519,14 +1519,24 @@ class XGBRegressorContainer(RegressorContainer):
             return
 
         from xgboost import XGBRegressor
+        
+        #If XGBoost > 2 change and add new parameters
+        xgboost_version = tuple(map(int, xgb.__version__.split('.')))
+        xgboost_2_or_higher = xgboost_version >= (2, 0, 0)
 
         args = {
             "random_state": experiment.seed,
             "n_jobs": experiment.n_jobs_param,
             "verbosity": 0,
-            "booster": "gbtree",
-            "tree_method": "gpu_hist" if experiment.gpu_param else "auto",
+            "booster": "gbtree",            
         }
+        
+        if xgboost_2_or_higher:
+            args["tree_method"] = "hist"
+            args["device"] = "cuda"
+        else:
+            args["tree_method"] = "gpu_hist" if experiment.gpu_param else "auto"
+            
         tune_args = {}
         tune_grid = {
             "learning_rate": [
