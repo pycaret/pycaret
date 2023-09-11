fef700c641f640fdb6ab13a6e50e64fa62e99078
Antoni Baum <antoni.baum@protonmail.com>
2023-09-11 02:30:36 +0000

    Remove Python 3.7 specific code
    Signed-off-by: Antoni Baum <antoni.baum@protonmail.com>

--- pycaret/__init__.py
+++ pycaret/__init__.py
@@ -1,20 +1,3 @@
-import os
-import sys
-
-# Fix for joblib on 3.7
-# Otherwise, it will default to cloudpickle which
-# uses pickle5, causing exceptions with joblib
-# THIS HAS TO BE DONE BEFORE OTHER IMPORTS HERE
-if sys.version_info < (3, 8):
-    import pycaret.internal.cloudpickle_compat
-
-    os.environ["LOKY_PICKLER"] = pycaret.internal.cloudpickle_compat.__name__
-
-    from joblib.externals.loky.backend.reduction import set_loky_pickler
-
-    set_loky_pickler(pycaret.internal.cloudpickle_compat.__name__)
-
-
 from pycaret.utils._show_versions import show_versions
 
 version_ = "3.1.0"
