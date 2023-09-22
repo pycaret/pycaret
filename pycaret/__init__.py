8f9351aa184f08f0a786a4eb0096c68bf2dda7ac
CelestinoXP <celestinoxp@hotmail.com>
2023-09-22 18:12:32 +0100

    Update __init__.py

--- pycaret/__init__.py
+++ pycaret/__init__.py
@@ -2,14 +2,15 @@ import sys
 
 from pycaret.utils._show_versions import show_versions
 
-# Pycaret only supports python 3.8.x up to 3.10.x
-if not (3.8 <= sys.version_info.major < 3.11):
-    raise Exception(
-        "PyCaret requires Python 3.8 to 3.10. Please upgrade your Python version."
-    )
-
 version_ = "3.1.0"
 
 __version__ = version_
 
 __all__ = ["show_versions", "__version__"]
+
+# Pycaret only supports python 3.8.x up to 3.10.x
+if not ((3, 8) <= sys.version_info < (3, 11)):
+    raise Exception(
+        f"PyCaret requires Python 3.8 to 3.10. Please upgrade your Python version."
+        f"Actual Python version: {sys.version_info}"
+    )
