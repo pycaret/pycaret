3dfe9198846e5b76d28cf48e063c63b5d24147de
CelestinoXP <celestinoxp@hotmail.com>
2023-09-22 17:05:22 +0100

    Fix python support
    This change is to avoid problems with versions of Python that are no longer supported such as Python 3.7

--- pycaret/__init__.py
+++ pycaret/__init__.py
@@ -1,4 +1,12 @@
+import sys
+
 from pycaret.utils._show_versions import show_versions
+
+# Pycaret only supports python 3.8 up to 3.10
+if not (3.8 <= sys.version_info.major <= 3.10):
+    raise Exception(
+        "PyCaret requires Python 3.8 to 3.10. Please upgrade your Python version."
+    )
 
 version_ = "3.1.0"
 
