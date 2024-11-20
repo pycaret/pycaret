d3d80d6fbe63118ba55fe7e1c2a122ae32eab152
Ayesha Shahzad <ayeshashahzad@Ayeshas-MacBook-Air.local>
2024-11-20 02:22:06 +0100

    refractor code

--- setup.py
+++ setup.py
@@ -1,63 +1,62 @@
-"""
-This is the setup script for the PyCaret library.
-"""
-
+"""Setup script for the PyCaret library."""
 
 # Copyright (C) 2019-2024 PyCaret
 # Author: Moez Ali (moez.ali@queensu.ca)
-# Contributors (https://github.com/pycaret/pycaret/graphs/contributors)
 # License: MIT
 
+from typing import Dict, List
 from setuptools import find_packages, setup
 
-
-def readme():
+def read_file(filename: str) -> str:
+    """Read and return contents of a file.
+    
+    Args:
+        filename: Path to the file to read
+        
+    Returns:
+        str: Contents of the file
     """
-    Reads the contents of the README.md file and returns it as a string.
+    with open(filename, encoding="utf8") as file:
+        return file.read()
+
+def get_requirements() -> Dict[str, List[str]]:
+    """Get all package requirements.
+    
+    Returns:
+        Dict containing main and optional requirements
     """
-    with open("README.md", encoding="utf8") as readme_file:
-        readme_content = readme_file.read()
-    return readme_content
+    required = read_file("requirements.txt").splitlines()
+    required_optional = read_file("requirements-optional.txt")
+    required_test = read_file("requirements-test.txt").splitlines()
+    required_dev = read_file("requirements-dev.txt").splitlines()
 
+    # Split optional requirements into categories
+    optional_sections = required_optional.split("\n\n")
+    extras_require = {
+        "analysis": optional_sections[0].splitlines(),
+        "models": optional_sections[1].splitlines(),
+        "tuners": optional_sections[2].splitlines(),
+        "mlops": optional_sections[3].splitlines(),
+        "parallel": optional_sections[4].splitlines(),
+        "test": required_test,
+        "dev": required_dev,
+    }
 
-with open("requirements.txt", encoding="utf8") as req_file:
-    required = req_file.read().splitlines()
+    # Combine all extras for 'full' installation
+    extras_require["full"] = sum(
+        [extras_require[key] for key in extras_require], 
+        []
+    )
 
-with open("requirements-optional.txt", encoding="utf8") as req_file:
-    required_optional = req_file.read()
+    return {"required": required, "extras": extras_require}
 
-with open("requirements-test.txt", encoding="utf8") as req_file:
-    required_test = req_file.read().splitlines()
-
-with open("requirements-dev.txt", encoding="utf8") as req_file:
-    required_dev = req_file.read().splitlines()
-
-
-extras_require = {
-    "analysis": required_optional.split("\n\n")[0].splitlines(),
-    "models": required_optional.split("\n\n")[1].splitlines(),
-    "tuners": required_optional.split("\n\n")[2].splitlines(),
-    "mlops": required_optional.split("\n\n")[3].splitlines(),
-    "parallel": required_optional.split("\n\n")[4].splitlines(),
-    "test": required_test,
-    "dev": required_dev,
-}
-
-extras_require["full"] = (
-    extras_require["analysis"]
-    + extras_require["models"]
-    + extras_require["tuners"]
-    + extras_require["mlops"]
-    + extras_require["parallel"]
-    + extras_require["test"]
-    + extras_require["dev"]
-)
-
+# Main setup configuration
+requirements = get_requirements()
 setup(
     name="pycaret",
     version="3.4.0",
     description="PyCaret - An open source, low-code machine learning library in Python.",
-    long_description=readme(),
+    long_description=read_file("README.md"),
     long_description_content_type="text/markdown",
     url="https://github.com/pycaret/pycaret",
     author="Moez Ali",
@@ -70,18 +69,17 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     packages=find_packages(include=["pycaret*"]),
     include_package_data=True,
-    install_requires=required,
-    extras_require=extras_require,
-    tests_require=required_test,
+    install_requires=requirements["required"],
+    extras_require=requirements["extras"],
+    tests_require=requirements["extras"]["test"],
     python_requires=">=3.9",
     project_urls={
         "Contributors": "https://github.com/pycaret/pycaret/graphs/contributors",
-        "Documentation": "https://pycaret.gitbook.io/docs",
+        "Documentation": "https://pycaret.gitbook.io/docs", 
         "Source": "https://github.com/pycaret/pycaret",
     },
 )
